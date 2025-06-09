a430fb5b4879bc358c20d35fb3ef2b571c4321b7
Alex <alexthefloff@gmail.com>
2025-06-09 17:50:30 +0300

    a

--- logit/school code/code.cs
+++ logit/school code/code.cs
@@ -15,37 +15,38 @@ namespace RectangleCalculator
             float floatsecond = float.parse(secondNumber);
 
             //teha pindala ja ümbermõõdu kalkulatsioon
-            
 
-            firstnumber * secondNumber
-            2* (firstnumber + secondNumber)
+
+            firstnumber* secondNumber;
+            2 * (firstnumber + secondNumber);
 
             //teha ruudu, kolmnurga ja ringi pindala ja ümbermõõdu
 
             Console.writeline("enter cube side value");
             string firstnumber2 = console.ReadLine();
             float floatfirst2 = float.parse(firstnumber2);
-            
-            firstnumber2^2
+
+            firstnumber2 ^ 2;
 
 
 
             Console.writeline("enter triangle first value");
             string firstnumber3 = console.ReadLine();
             float floatfirst3 = float.parse(firstnumber3);
-            
+
             Console.writeline("enter triangle second value");
             string secondnumber3 = console.ReadLine();
             float floatsecond3 = float.parse(secondnumber3);
-            
+
             Console.writeline("enter triangle third value");
             string thirdnumber3 = console.ReadLine();
             float floatthird3 = float.parse(thirdnumber3);
-            
-            firstnumber3 + secondnumber3 +thirdnumber3
 
-            
-            Console.writeline("enter triangle hight");
+            firstnumber3 + secondnumber3 + thirdnumber3;
+
+
+
+            Console;writeline("enter triangle hight");
             string firstnumber4 = console.ReadLine();
             float floatfirst4 = float.parse(firstnumber4);
 
@@ -54,16 +55,16 @@ namespace RectangleCalculator
             float floatfsecond4 = float.parse(secondnumber4);
 
 
-            firstnumber4 * secondnumber4
+            firstnumber4* secondnumber4;
 
 
             Console.writeline("enter sircle radius value");
             string firstnumber5 = console.ReadLine();
             float floatfirst5 = float.parse(firstnumber5);
 
-            2 * firstnumber5 * math.pi
+            2 * firstnumber5 * math.pi;
 
-            firstnumber5^2 * math.pi
+            firstnumber5 ^ 2 * math.pi;
         }
     }
 }
