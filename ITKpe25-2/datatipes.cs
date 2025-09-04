a52f0eb0192e8fbaad8d3451afda94a7ec5c3628
Alex <alexthefloff@gmail.com>
2025-09-04 10:54:07 +0300

    start

---
+++ ITKpe25-2/datatipes.cs
@@ -0,0 +1,30 @@
+namespace datatipes
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("hello, world");
+
+            //bool  andmetuup toetab ainult true ja false
+            bool check = true;
+            bool negative = false;
+
+            //string
+            //string on tahemark
+            //koike saab sisestada mis on klaviatuuril
+            string mark = "123";
+            string mark2 = "123";
+            console.Writeline(mark + mark2);
+
+            //int
+            //teha kalkulatsioon, kus muutuja taisarv liidetakse
+            //taisarv 2-ga
+            // tulemus peab naitama konsoolis
+            int taisarv = 123;
+            console.Writeline(taisarv * 2);
+
+            
+        }
+    }
+}
---
+++ ITKpe25-2/intro.cs
@@ -0,0 +1,17 @@
+//namespace on selleks, et naha projekti nime
+//tegemist on projekti aadressiga
+namespace intro
+{
+    //classi nimi
+    //internal tahendab classi ligipaasutaset
+    internal class Program
+    {
+        //see on meetod nimega main
+        static void Main(string[] args)
+        {
+            //see on komentaar ja seda ei loeta koodi kaivitamisel
+            //kommentaari saab igasususeid asju kirjutada
+            Console.Writeline("hello, world");
+        }
+    }
+}
