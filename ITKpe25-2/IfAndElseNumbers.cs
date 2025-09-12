e2e3ea8269eefb68e67bf5681fc6fee1a838fc3a
Alex <alexthefloff@gmail.com>
2025-09-12 10:35:59 +0300

    a

--- ITKpe25-2/IfAndElseNumbers.cs
+++ ITKpe25-2/IfAndElseNumbers.cs
@@ -6,15 +6,28 @@ namespace IfAndElseNumbers
         {
             Console.Writeline("hello, world");
             //konsoolis on ainult string andmetuup
-            string number = console.ReadLine();
 
-            if(number != null) 
+            // defineerime ara muutuja nimega number
+            string number = console.ReadLine();
+            // konverteerime muutuja number ara numberConverteriks
+            try
             {
-                System.Console.WriteLine("sisesta enda vanuse: " + numbers);
+                int numberConverted = Convert.ToInt32(numbers);
             }
+            catch (Expextasion e)
+            {
+                numberConverted = 0;
+            }
+
+            // toimub kontroll, et kas muutujal on vaartus
+            if (numberConverted > 0)
+            {
+                System.Console.WriteLine("sisesta enda vanuse: " + numbersConverted);
+            }
+            // kui ei sisesta numbri vaartust, siis on teada: ei ole vanust
             else
             {
-                System.Console.WriteLine("err no numbreid");
+                System.Console.WriteLine("err no vanust");
             }
         }
     }
