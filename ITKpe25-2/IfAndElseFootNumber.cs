fd9c8824f505802c3ea169012fd6960c5f266f1c
alex <133362511+alexthefloff@users.noreply.github.com>
2025-09-19 10:43:42 +0300

    Update IfAndElseFootNumber.cs

--- ITKpe25-2/IfAndElseFootNumber.cs
+++ ITKpe25-2/IfAndElseFootNumber.cs
@@ -7,27 +7,27 @@ namespace IfAndElseFootNumbers
             Console.Writeline("hello, world");
 
             System.Console.WriteLine("what be thy foot size");
-            size = Console.ReadLine();
+            int(size = Console.ReadLine());
 
-            if (size <= 30 && size >= 33)
+            if (size >= 30 && size <= 33)
             {
-
+                Console.Writeline("size 30-33")
             }
-            else if (size <= 34 && size >= 38)
+            else if (size >= 34 && size <= 38)
             {
-
+                Console.Writeline("size 34-38")
             }
-            else if (size <= 39 && size >= 44)
+            else if (size >= 39 && size <= 44)
             {
-
+                Console.Writeline("size 39-44")
             }
-            else if (size <= 45 && size >= 48)
+            else if (size >= 45 && size <= 48)
             {
-
+                Console.Writeline("size 45-48")                
             }
             else
             {
-                
+                Console.Writeline("a valid size was not entered")
             }
         }
     }
