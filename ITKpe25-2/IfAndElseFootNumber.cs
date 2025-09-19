c0a8752471da23c296c04e6fd7654ea2f7c5fd4b
Alex <alexthefloff@gmail.com>
2025-09-19 10:23:36 +0300

    a

--- ITKpe25-2/IfAndElseFootNumber.cs
+++ ITKpe25-2/IfAndElseFootNumber.cs
@@ -5,6 +5,19 @@ namespace IfAndElseFootNumbers
         static void Main(string[] args)
         {
             Console.Writeline("hello, world");
+
+            System.Console.WriteLine("what be thy foot size");
+            size = Console.ReadLine();
+
+            if (size <= 30 && size >= 33)
+            {
+
+            }
+            else if (size <= 30 && size >= 33)
+            {
+
+            }
+            
         }
     }
 }
