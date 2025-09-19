cbff41317f12fac5d29ff0b7d56d6038c45d171c
alex <133362511+alexthefloff@users.noreply.github.com>
2025-09-19 10:58:38 +0300

    Update IfAndElseFootNumber.cs

--- ITKpe25-2/IfAndElseFootNumber.cs
+++ ITKpe25-2/IfAndElseFootNumber.cs
@@ -11,18 +11,22 @@ namespace IfAndElseFootNumbers
 
             if (size >= 30 && size <= 33)
             {
+                Console.ForegroundColor = ConsoleColor.Green
                 Console.Writeline("size 30-33")
             }
             else if (size >= 34 && size <= 38)
             {
+                Console.ForegroundColor = ConsoleColor.White
                 Console.Writeline("size 34-38")
             }
             else if (size >= 39 && size <= 44)
             {
+                Console.ForegroundColor = ConsoleColor.Yellow
                 Console.Writeline("size 39-44")
             }
             else if (size >= 45 && size <= 48)
             {
+                Console.beep
                 Console.Writeline("size 45-48")                
             }
             else
