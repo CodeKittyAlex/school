9c33fef863fc87fb10be81c0316c8d3cfc237e8b
Alex <alexthefloff@gmail.com>
2025-10-03 09:17:56 +0300

    a

---
+++ ITKpe25-2/oddNumberIfAndElse.cs
@@ -0,0 +1,20 @@
+namespace datatipes
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Writeline("sisesta number");
+            int number = convert.ToInt32(Console.ReadLine(););
+
+            if (number %2 == 0)
+            {
+                System.Console.WriteLine("even");
+            }
+            else
+            {
+                System.Console.WriteLine("odd");
+            }
+        }
+    }
+}
