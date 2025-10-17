823e9fe3d363342ce7c97fc952cb35db9afc311d
alex <alexthefloff@gmail.com>
2025-10-17 09:53:06 +0300

    switch lesson

---
+++ ITKpe25-2/program.cs
@@ -0,0 +1,31 @@
+namespace program
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            console.WriteLine("sisesta vokaal ja vajuta enter");
+            string input = Console.readLine();
+
+            switch (input)
+            {
+                case "a":
+                    System.Console.WriteLine("vokaal a");
+                    break;
+                
+                case "e":
+                    System.Console.WriteLine("vokaal e");
+                    break;
+
+                case "i":
+                    System.Console.WriteLine("vokaal i");
+                    break;
+                
+                default:
+                    System.Console.WriteLine("muu tähr");
+                    break;
+            }
+        }
+    }
+}
+ 
