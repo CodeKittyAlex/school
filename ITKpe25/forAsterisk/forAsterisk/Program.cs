1cfa892905c403cbb5a6b1105b6acab434ea5f5c
Opilane TTHK <133362511+CodeKittyAlex@users.noreply.github.com>
2025-12-05 10:33:02 +0200

    small change

---
+++ ITKpe25/forAsterisk/forAsterisk/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace forAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for Asterisk");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine("*");
+                
+            }
+        }
+    }
+}
