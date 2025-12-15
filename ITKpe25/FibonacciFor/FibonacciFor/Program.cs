1e68710a007f89e3148b0780cf4d205deb448458
Opilane TTHK <133362511+CodeKittyAlex@users.noreply.github.com>
2025-12-15 09:58:53 +0200

    lesson

---
+++ ITKpe25/FibonacciFor/FibonacciFor/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\n");
+            Console.Write("Näita esimest Fibonacci seeriat");
+            Console.WriteLine("--------------------------------\n\n");
+            Console.WriteLine("sisesta number");
+            int prv = 0, pre = 1, trm, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"siin on Fibonacci seeria{0} kuni \n", n);
+
+            Console.Write($"{0} {1}", prv, pre);
+            Console.Write("\n\n");
+
+            for (int i = 3; i <= n; i++)
+            {
+                trm = prv + pre;   
+                Console.WriteLine(trm);
+                prv = pre;
+                pre = trm;
+                
+            }
+        }
+    }
+}
---
+++ ITKpe25/decimalToBinary/decimalToBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace decimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal to binary");
+
+            Console.WriteLine("sisesta number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string binaryNumber = "";
+
+            while (num > 0)
+            {
+                int reminder = num % 2; 
+                binaryNumber = reminder + binaryNumber;
+                num /= 2;
+
+            }
+            Console.WriteLine(binaryNumber);
+        }
+    }
+}
